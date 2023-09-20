dd1a6576f3690722b275ca2e70522c3aad424a51
Jinsu Kwak <jinsukwak2001@gmail.com>
2023-09-20 00:17:28 -0600

    add login/register page

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -2,12 +2,14 @@
 import Button from "@/components/Button";
 import Input from "@/components/Input";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
-import { signIn } from "next-auth/react";
+import axios from "axios";
 
-const LoginPage = () => {
+const RegisterPage = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
 
   const {
     register,
@@ -15,6 +17,7 @@ const LoginPage = () => {
     formState: { errors },
   } = useForm<FieldValues>({
     defaultValues: {
+      name: "",
       email: "",
       password: "",
     },
@@ -23,7 +26,8 @@ const LoginPage = () => {
   const onSubmit: SubmitHandler<FieldValues> = async (body) => {
     setIsLoading(true);
     try {
-      const data = signIn("credentials", body);
+      const { data } = await axios.post("/api/register", body);
+      router.push("/auth/login");
     } catch (error) {
       console.log(error);
     } finally {
@@ -34,8 +38,9 @@ const LoginPage = () => {
   return (
     <section className="grid h-[calc(100vh_-_56px)] place-items-center">
       <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col justify-center gap-4 min-w-[350px]">
-        <h1 className="text-2xl">Login</h1>
+        <h1 className="text-2xl">Register</h1>
         <Input id="email" label="Email" disabled={isLoading} register={register} errors={errors} required />
+        <Input id="name" label="Name" disabled={isLoading} register={register} errors={errors} required />
         <Input
           id="password"
           label="Password"
@@ -46,12 +51,12 @@ const LoginPage = () => {
           required
         />
 
-        <Button label="Login" />
+        <Button label="Register" />
         <div className="text-center">
           <p className="text-gray-400">
-            Not a member?{" "}
-            <Link href="/auth/register" className="text-black hover:underline">
-              Register
+            Already a member?{" "}
+            <Link href="/auth/login" className="text-black hover:underline">
+              Login
             </Link>
           </p>
         </div>
@@ -60,4 +65,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default RegisterPage;
