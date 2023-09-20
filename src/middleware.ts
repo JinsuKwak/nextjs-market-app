f7817b01a18a7a750a5ca259a7a91f5941e54387
Jinsu Kwak <jinsukwak2001@gmail.com>
2023-09-19 23:50:21 -0600

    add reusable button/input component

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/admin/:path*", "/user/:path*"] };
+// export const config = { matcher: ["/admin/:path*", "/user/:path*"] };
+
+export async function middleware(req: NextRequest) {
+  const session = await getToken({ req, secret: process.env.JWT_SECRET });
+  const pathname = req.nextUrl.pathname;
+
+  // logged in user
+  if (pathname.startsWith("/user") && !session) {
+    return NextResponse.redirect(new URL("/auth/login", req.url));
+  }
+
+  // admin user
+  if (pathname.startsWith("/admin") && session?.role !== "Admin") {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  // redirect logged in user from signin/login page
+  if (pathname.startsWith("/auth") && session) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  return NextResponse.next();
+}
